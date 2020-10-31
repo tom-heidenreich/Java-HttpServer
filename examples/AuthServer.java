28ee0f6c0f5744400e4a377e88feaef6aa9bd39b
Txmyx <53293648+Toom-boop@users.noreply.github.com>
2020-10-31 15:57:55 +0100

    Update AuthServer.java

--- examples/AuthServer.java
+++ examples/AuthServer.java
@@ -1,30 +1,32 @@
-public static void main(String[] args) throws IOException{
-	    HttpServer server = new HttpServer(new InetSocketAddress(80), 0);
-	    
-	    AuthenticatonHandler authHandler = new AuthenticatonHandler((user)->{
-				if(user.getUsername().equals("Username") && user.getPassword().equals("Password")) {
-					return true;
-				}else {
-					return false;
-				}
+public class AuthServer {
+	public static void main(String[] args) throws IOException{
+		HttpServer server = new HttpServer(new InetSocketAddress(80), 0);
+
+		AuthenticatonHandler authHandler = new AuthenticatonHandler((user)->{
+			if(user.getUsername().equals("Username") && user.getPassword().equals("Password")) {
+				return true;
+			}else {
+				return false;
+			}
+		});
+
+		authHandler.setFailedAuthAction((req, res)->{
+			System.out.println("Authentication failed: " + req.getRemoteAddress());
+			res.writeHead(200, (head)->{
+				head.setHeader("Content-Type", "text/plain");
 			});
-			
-			authHandler.setFailedAuthAction((req, res)->{
-				System.out.println("Authentication failed: " + req.getRemoteAddress());
-				res.writeHead(200, (head)->{
-					head.setHeader("Content-Type", "text/plain");
-				});
-				res.write("Failed!");
-				res.end();
+			res.write("Failed!");
+			res.end();
+		});
+
+		server.addHandler("/login", (req, res)->{
+			res.writeHead(200, (head)->{
+				head.setHeader("Content-Type", "text/plain");
 			});
-	  
-	  server.addHandler("/login", (req, res)->{
-		  res.writeHead(200, (head)->{
-			  head.setHeader("Content-Type", "text/plain");
-		  });
-		  res.write("Login");
-		  res.end();
-	  }).setAuthenticator(authHandler);
-	  
-	  server.start();
+			res.write("Login");
+			res.end();
+		}).setAuthenticator(authHandler);
+
+		server.start();
 	}
+}
